d776b8b9342806bf4b594b32d281ba6601c97a1c
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-12 11:22:09 -0700

    feat: add MapHandler function

--- URL_Shortener/handler.go
+++
@@ -1,9 +0,0 @@
-package urlshort
-
-import (
-	"net/http"
-)
-
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-
-}
---
+++ URL_Shortener/urlshort/handler.go
@@ -0,0 +1,24 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+// MapHandler is a function that's called whenever the server receives a request.
+// The MapHandler is a closure; it has access to request details (http.Request).
+// It extracts the URL from this request and checks if it's in the map; if it is,
+// it redirects to corresponding URL; if not, it will fallback to fallback handler.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Extract the URL from the request
+		requestedURL := r.URL.Path
+
+		// Check if the requestedURL is in map
+		if url, ok := pathsToUrls[requestedURL]; ok {
+			http.Redirect(w, r, url, http.StatusFound)
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
