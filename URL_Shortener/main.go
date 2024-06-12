e46aafee65072b34ef09f4a5ba0ad91f185ca241
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-12 12:07:51 -0700

    refactor: add spacing to function

--- URL_Shortener/main.go
+++ URL_Shortener/main.go
@@ -15,9 +15,10 @@ func main() {
 		"/cat": "www.samplesite.com/article-on-cats",
 	}
 
-	fallback := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page not found", http.StatusNotFound)
-	})
+	fallback := http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "Page not found", http.StatusNotFound)
+		})
 
 	urlshort.MapHandler(pathsToUrls, fallback)
 }
