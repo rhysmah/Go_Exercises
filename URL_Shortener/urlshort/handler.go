a674def59e4738024d687b92f89ea3e522fb02ef
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-13 10:21:08 -0700

    refactor: add JSON parser and handler; generalize `buildURLMap`

--- URL_Shortener/urlshort/handler.go
+++ URL_Shortener/urlshort/handler.go
@@ -1,15 +1,17 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"gopkg.in/yaml.v3"
 )
 
+// Uses multiple tags for YAML and JSON
 type PathURL struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
+	Path string `yaml:"path" json:"path"`
+	URL  string `yaml:"url" json:"url"`
 }
 
 // MapHandler is a function that's called whenever the server receives a request.
@@ -39,6 +41,15 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	return MapHandler(mappedData, fallback), nil
 }
 
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedData, err := parseJSON(jsn)
+	if err != nil {
+		return nil, err
+	}
+	mappedData := buildURLMap(parsedData)
+	return MapHandler(mappedData, fallback), nil
+}
+
 // Helper function: unmarshals YAML data into a struct slice
 func parseYAML(yml []byte) ([]PathURL, error) {
 	var yamlData []PathURL
@@ -49,12 +60,22 @@ func parseYAML(yml []byte) ([]PathURL, error) {
 	return yamlData, nil
 }
 
-// Helpre function: builds a map of paths and URLs from a struct slice
-func buildURLMap(parsedYAML []PathURL) map[string]string {
+// Helper functino: unmarshals JSON data into a struct slice
+func parseJSON(jsn []byte) ([]PathURL, error) {
+	var jsonData []PathURL
+	err := json.Unmarshal(jsn, &jsonData)
+	if err != nil {
+		return nil, fmt.Errorf("cannot unmarshal JSON data: %v", err)
+	}
+	return jsonData, nil
+}
+
+// Helper function: builds a map of paths and URLs from a struct slice
+func buildURLMap(parsedData []PathURL) map[string]string {
 	pathsToURLS := make(map[string]string)
 
 	// Iterate over the slice of PathURL and populate map with path: url
-	for _, pathURL := range parsedYAML {
+	for _, pathURL := range parsedData {
 		pathsToURLS[pathURL.Path] = pathURL.URL
 	}
 	return pathsToURLS
