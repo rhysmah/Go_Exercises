ab4eaad67f1ee5868182a95fb1959f65e13779b1
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-17 14:58:03 -0700

    chore: add client-side message about server starting

--- Adventure_Game/webserver/webserver.go
+++ Adventure_Game/webserver/webserver.go
@@ -37,6 +37,7 @@ func storyHandler(w http.ResponseWriter, r *http.Request) {
 
 func StartServer() {
 	http.HandleFunc("/story", storyHandler)
+	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Error starting server: ", err)
