d0371f3f196ac2b739f8a48a9c968aa0d56d4d9c
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-17 14:57:21 -0700

    refactor: call `StartServer` from `main`; remove other code

--- Adventure_Game/main.go
+++ Adventure_Game/main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"adventure/jsonhandler"
-	"fmt"
-	"log"
+	"adventure/webserver"
 )
 
 func main() {
-	content, err := jsonhandler.ReadFile("gopher.json")
-	if err != nil {
-		log.Fatal("Error reading file: ", err)
-	}
-
-	storyData, err := jsonhandler.ParseJSON(content)
-	if err != nil {
-		log.Fatal("Error parsing JSON data: ", err)
-	}
-
-	fmt.Println(storyData)
+	webserver.StartServer()
 }
