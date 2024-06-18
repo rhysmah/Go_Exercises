5f72a1e3bfb14953513402d59d70fb78f6d9b214
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-18 12:56:03 -0700

    feat: add components

--- Adventure_Game/frontend/adventure-game/src/App.js
+++ Adventure_Game/frontend/adventure-game/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
+import ContentBox from './components/ContentBox';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Provide content!
-        </p>
+        <ContentBox />
       </header>
     </div>
   );
---
+++ Adventure_Game/frontend/adventure-game/src/components/ContentBox.jsx
@@ -0,0 +1,9 @@
+function ContentBox() {
+    return (
+        <div className="content-box">
+            <h1>Content Box</h1>
+        </div>
+    )
+}
+
+export default ContentBox;
