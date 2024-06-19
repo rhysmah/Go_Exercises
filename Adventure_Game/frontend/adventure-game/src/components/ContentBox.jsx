2d1c6b198d21958df3e63fe70cbeede27d36fa49
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-06-18 17:35:43 -0700

    refactor: streamlined data-fetching for component

--- Adventure_Game/frontend/adventure-game/src/components/ContentBox.jsx
+++ Adventure_Game/frontend/adventure-game/src/components/ContentBox.jsx
@@ -1,38 +1,41 @@
 import React, {useEffect, useState} from 'react';
 
-// Call backend API
 const ContentBox = () => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        console.log("Component mounted; fetching data...")
+    useEffect( () => {
+        console.log("Component mounted. Fetching data...")
         fetch('http://localhost:8080/story')
         .then(response => {
-            console.log("Received response: ", response)
-
             if (!response.ok) {
-                throw new Error('Failed to fetch data')
+                setError('Failed to fetch data')
             }
             return response.json()
         })
         .then(data => setData(data))
-        .catch(error => setError(error));
-    }, []);
+        .catch(error => setError(error))
+    })
 
-    if (error) {
-        return <p>{error.message}</p>
-    }
-
-    if (!data) {
-        return <p>Loading...</p>
-    }
+    if (error) return <p>Error: {error}</p>
+    if (!data) return <p>Loading...</p>
 
     return (
-        <div className="content-box">
-            <h1>{data.intro.title}</h1>
-            <p>{data.intro.story}</p>
-        </div>
+        <>
+            <div className="content-box">
+                <h1>{data.intro.title}</h1>
+                <p>{data.intro.story}</p>
+            </div>
+
+            <div className="links-box">
+                <ul>
+                    {data.intro.options.map((option, index) => (
+                        <li key={index}><a href={option.link}>{option.text}</a></li>
+                    ))}
+                </ul>
+            </div>
+
+        </>
     )
 }
 
